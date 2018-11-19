5183ee74f4f6b6183f93f41de3ff18e7e0d153b5
LoperDog <LoperDog10@gmail.com>
2018-11-19 16:32:19 +0900

    FirstCommit
    ECS Systems FirstCommit

---
+++ ECS/TestMain.cpp
@@ -0,0 +1,117 @@
+﻿#pragma once
+#pragma warning(disable:4996)
+
+
+#include "ECS.h"
+#include <stdio.h>
+#include <conio.h>
+
+
+using namespace std;
+
+// 기본형 위치 컴포넌트
+class Position : public JT::Component {
+public :
+  Position() : X(0), Y(0) {}
+  Position(int x_, int y_) : X(x_), Y(y_) {}
+
+  void SetPosition(int x_, int y_) {
+    X = x_; Y = y_;
+  }
+  void SetX(int x_) { X = x_; }
+  void SetY(int y_) { Y = y_; }
+  int GetX() { return X; }
+  int GetY() { return Y; }
+
+private :
+  int X, Y;
+};
+
+// 유저 컨트롤러 예시
+class Controller : public JT::Component {
+public :
+  Controller() : R(1), U(10){}
+  Controller(int r_, int u_) : R(r_), U(u_) {}
+
+  int GetR() { return R; }
+  int GetL() { return -R; }
+  int GetU() { return U; }
+  int GetD() { return -U; }
+
+private :
+  int R, U;
+};
+
+
+// 중력 엔티티시스템
+class Gravity : public JT::EntitySystem {
+public :
+  Gravity(std::shared_ptr<JT::World> world_) : JT::EntitySystem(world_) {}
+  void tick() override {
+    //std::cout << "Start Gravity Tick " << std::endl;
+    world_.get()->each<Position>([&](std::shared_ptr<JT::Entity> ent) {
+      auto pos = ent->GetComponent<Position>().get();
+      if (pos->GetY() > 0) {
+        pos->SetY(pos->GetY() - 1);
+      }
+    });
+  }
+  std::type_index GetTypeIndex() override {
+    return std::type_index(typeid(this)); 
+  }
+
+};
+
+// 키입력 시스템
+class User : public JT::EntitySystem {
+public:
+  User(std::shared_ptr<JT::World> world_) : JT::EntitySystem(world_) {}
+  void tick() override {
+    world_.get()->each<Controller>([&](std::shared_ptr<JT::Entity> ent) {
+      auto pos = ent->GetComponent<Position>().get();
+      auto con = ent->GetComponent<Controller>().get();
+      char a;
+      int b;
+      a = getch();
+      b = int(a);
+      if (b == 75) {
+        pos->SetX(pos->GetX() + con->GetL());
+      }
+      else if (b == 77) {
+        pos->SetX(pos->GetX() + con->GetR());
+      }
+      else if (b == 72) {
+        pos->SetY(pos->GetY() + con->GetU());
+      }
+      else if (b == 80) {
+        pos->SetY(pos->GetY() + con->GetD());
+      }
+    });
+  }
+  std::type_index GetTypeIndex() override {
+    return std::type_index(typeid(this));
+  }
+};
+
+void main() {
+  std::shared_ptr<JT::World> world = std::make_shared<JT::World>();
+  std::shared_ptr<Gravity> gri = std::make_shared<Gravity>(world);
+  std::shared_ptr<User> use = std::make_shared<User>(world);
+  use->SetType(SYSTEM_TYPE_SYNC);
+  world->AddSystem(gri);
+  world->AddSystem(use);
+
+  auto user = world->create();
+  user->SetName("User");
+  user.get()->AddComponent(std::make_shared<Position>());
+  user.get()->AddComponent(std::make_shared<Controller>());
+
+  auto Mon = world->create();
+  Mon->SetName("Monster");
+  Mon.get()->AddComponent(std::make_shared<Position>());
+
+  while (true) {
+    //std::cout << "World Tick" << std::endl;
+    world->tick();
+  }
+}
---
+++ ECS/ThreadPool.h
@@ -0,0 +1,89 @@
+﻿#ifndef ThreadPool_H
+#define ThreadPool_H
+
+#include <algorithm>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
+#include <iostream>
+
+namespace JT {
+  class ThreadPool{
+  public:
+    ThreadPool() : stop(false) { this->MakeThread(8); }
+    ThreadPool(size_t _s) :stop(false) { this->MakeThread(_s); }
+
+    void enqueue(std::function<void()>);
+
+    ~ThreadPool();
+
+    void join();
+
+  private:
+    void MakeThread(size_t);
+    // 일을 할 쓰레드들
+    std::vector<std::thread> workers_;
+    // 타스크
+    std::queue<std::function<void()>> task_;
+    
+    std::condition_variable worker_condition;
+    std::mutex task_mutex;
+
+    bool stop;    
+  };
+
+  inline void ThreadPool::MakeThread(size_t _s) {
+    for (size_t i = 0; i < _s; i++) {
+      workers_.emplace_back([this] {
+        for (;;)
+        {
+          std::function<void()> task;
+          {
+            std::unique_lock<std::mutex> lock(this->task_mutex);
+
+            this->worker_condition.wait(lock,
+              [this] { return this->stop || !this->task_.empty(); });
+
+
+            if (this->stop && this->task_.empty()) {
+              return;
+            }
+            task = std::move(this->task_.front());
+            this->task_.pop();
+          }
+          task();
+        }
+      });
+    }
+  }
+
+  void ThreadPool::enqueue(std::function<void()> _func) {
+    {
+      std::unique_lock<std::mutex> lock(task_mutex);
+      task_.emplace(_func);
+    }
+    worker_condition.notify_one();
+  }
+
+  inline ThreadPool::~ThreadPool() {
+    std::cout << "소멸자 실행" << std::endl;
+    {
+      std::unique_lock<std::mutex> lock(task_mutex);
+      stop = true;
+    }
+    worker_condition.notify_all();
+    for (std::thread &worker : workers_)
+      worker.join();
+  }
+  inline void ThreadPool::join() {
+    while (!task_.empty()){}
+    return;
+  }
+}
+#endif // !ThreadPool_H
